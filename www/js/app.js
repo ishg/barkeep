c6bef2393f35d0d866ea655e5512928f887cc634
Koding <ishmeetsinghgrewal@gmail.com>
2015-07-24 20:16:10 +0000

    Locations and Days are being queried from Parse. Spinner shows progress

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 // Ionic Starter App
-
+Parse.initialize("euJpAsFf8PjtGTNY0104aq6iLq4YvlGp4TNHhrhl", "3GUvRwkxNXuMFA3x6GYNwnIwq8Ml4bzQ4J6OcY6A");
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
@@ -11,7 +11,8 @@ angular.module('starter', [
   'ionic.service.core',
   'ionic.service.push',
   'ionic.service.deploy',
-  'starter.controllers'
+  'starter.controllers',
+  'starter.services'
 ])
 
 .config(['$ionicAppProvider', function($ionicAppProvider) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,137 +4,37 @@ angular.module('starter.controllers', [])
   // Nothing to see here.
 })
 
-.controller('LocationsCtrl', function($scope, $rootScope, Locations ) {
-    $scope.locations = Locations.all();
-    console.log($scope.locations);
-})
-
-.controller('LocationDetailCtrl', function($scope, $stateParams, Locations){
-    $scope.location = Locations.get($stateParams.locId);
-})
-
-.controller('DaysCtrl', function($scope, Days) {
-    $scope.days = Days.all();
-})
-
-.controller('DayDetailCtrl', function($scope, $stateParams, Days){
-    $scope.day = Days.get($stateParams.dayId);
-})
-
-.factory('Days', function(){
-    var days = [
-        {id: 1, name: 'Monday'},
-        {id: 2, name: 'Tuesday'},
-        {id: 3, name: 'Wednesday'},
-        {id: 4, name: 'Thursday'},
-        {id: 5, name: 'Friday'},
-        {id: 6, name: 'Saturday'},
-        {id: 7, name: 'Sunday'}
-    ];
+.controller('LocationsCtrl', function($scope, $rootScope, LocationsService, $ionicLoading ) {
+    $ionicLoading.show({
+      template: '<ion-spinner></ion-spinner>'
+    });
     
-    return{
-        all: function(){
-            return days;
-        },
-        get: function(dayId){
-            return days[dayId-1];
-        }
-    }
+    LocationsService.all().then(function(result){
+        $scope.locations = result;
+        $ionicLoading.hide();
+    }, function(err){
+        console.log(err);
+    })
 })
 
-.factory('Locations', function(){
-    var locations = [
-        {
-            id: 1,
-            name: 'Bier Stube',
-            address: '',
-            description: ''
-        },
-        {
-            id: 2,
-            name: "Eddie George's Grill",
-            address: '',
-            description: ''
-        },
-        {
-            id: 3,
-            name: 'Ethyl & Tank',
-            address: '',
-            description: ''
-        },
-        {
-            id: 4,
-            name: 'Formaggio',
-            address: '',
-            description: ''
-        },
-        {
-            id: 5,
-            name: '4th Street Bar & Grill',
-            address: '',
-            description: ''
-        },
-        {
-            id: 6,
-            name: "Ledo's Tavern",
-            address: '',
-            description: ''
-        },
-        {
-            id: 7,
-            name: 'Mad Mex',
-            address: '',
-            description: ''
-        },
-        {
-            id: 8,
-            name: 'Out-R-Inn',
-            address: '',
-            description: ''
-        },
-        {
-            id: 9,
-            name: "Skully's",
-            address: '',
-            description: ''
-        },
-        {
-            id: 10,
-            name: 'Suzie-Cue',
-            address: '',
-            description: ''
-        },
-        {
-            id: 11,
-            name: 'The Little Bar',
-            address: '',
-            description: ''
-        },
-        {
-            id: 12,
-            name: 'Ugly Tuna',
-            address: '',
-            description: ''
-        },
-        {
-            id: 13,
-            name: 'Village Idiot',
-            address: '',
-            description: ''
-        }
-    ];
-    
-    return{
-        all: function(){
-            return locations;
-        },
-        get: function(locId){
-            return locations[locId-1];
-        }
-    }
+.controller('LocationDetailCtrl', function($scope, $stateParams, LocationsService){
+    $scope.location = LocationsService.get($stateParams.locId);
+    console.log($scope.location);
 })
 
-.factory('Specials', function(){
-   
+.controller('DaysCtrl', function($scope, DaysService, $ionicLoading) {
+    $ionicLoading.show({
+      template: '<ion-spinner></ion-spinner>'
+    });
     
-});
+    DaysService.all().then(function(result){
+        $scope.days = result;
+        $ionicLoading.hide();
+    }, function(err){
+        console.log(err);
+    })
+})
+
+.controller('DayDetailCtrl', function($scope, $stateParams, DaysService){
+    $scope.day = DaysService.get($stateParams.dayId);
+})
